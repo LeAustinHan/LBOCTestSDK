06a05de8dc7c5b5724419f54b32ba9dbac3497dc
LeAustinHan <314456527@qq.com>
2019-11-29 18:07:23 +0800

    rename dir

---
+++ LBOCTestSDKCode/Classes/LBOC_Test.m
@@ -0,0 +1,17 @@
+//
+//  LBOC_Test.m
+//  LBOCFirstDemo
+//
+//  Created by Han Jize on 2019/11/29.
+//  Copyright © 2019 LBOC. All rights reserved.
+//
+
+#import "LBOC_Test.h"
+
+@implementation LBOC_Test
+
+- (void)testFunction:(NSString *)infoString{
+    NSLog(@"test project == %@",infoString);
+}
+
+@end
