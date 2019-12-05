49dac41d9ef399b4ac160a770805f56110f0cd8d
hanjize <314456527@qq.com>
2019-12-05 16:39:30 +0800

    fix new file

---
+++ LBOCTestSDKCode/BaiduLocationMap/LBOC_Test.m
@@ -0,0 +1,13 @@
+//
+//  LBOC_Test.m
+//  LBOCFirstDemo
+//
+//  Created by Han Jize on 2019/11/29.
+//  Copyright © 2019 LBOC. All rights reserved.
+//
+
+#import "LBOC_Test.h"
+
+@implementation LBOC_Test
+
+@end
