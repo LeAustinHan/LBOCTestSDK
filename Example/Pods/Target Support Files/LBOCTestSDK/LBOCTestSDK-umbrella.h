36efcae4946935314bf04d6134c029c55cfb8fc9
hanjize <314456527@qq.com>
2020-06-11 16:33:13 +0800

    fix pod

---
+++ Example/Pods/Target Support Files/LBOCTestSDK/LBOCTestSDK-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSDate+Ext.h"
+
+FOUNDATION_EXPORT double LBOCTestSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char LBOCTestSDKVersionString[];
+
